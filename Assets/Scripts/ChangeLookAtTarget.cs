3bb3bb658dbc639b5cbecb1658bdb45b7670811e
papakosh <brian.navarro@gmail.com>
2016-04-19 10:37:44 -0500

    cleaning

---
+++ Assets/Scripts/ChangeLookAtTarget.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeLookAtTarget : MonoBehaviour {
+
+	public GameObject target; // the target that the camera should look at
+	public AudioClip nameSFX;
+	void Start() {
+		if (target == null) 
+		{
+			target = this.gameObject;
+			Debug.Log ("ChangeLookAtTarget target not specified. Defaulting to parent GameObject");
+		}
+	}
+
+	// Called when MouseDown on this gameObject
+	void OnMouseDown () {
+		// change the target of the LookAtTarget script to be this gameobject.
+		LookAtTarget.target = target;
+		Camera.main.fieldOfView = 60*target.transform.localScale.x;
+
+		// play name sfx if exists
+		if (nameSFX)
+			AudioSource.PlayClipAtPoint (nameSFX, target.transform.position, 2.0f);
+	}
+}
